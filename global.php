ed618532872c8e6ce00d611649d60812e1dbac2a
trungtin011 <169126046+trungtin011@users.noreply.github.com>
2024-12-02 08:26:33 +0700

    delete

--- global.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $ROOT_URL = "/Project-Duan1-mai";
-    $CONTENT_URL = "$ROOT_URL/View";
-    $MODAL_URL = "$ROOT_URL/Modal";
-    $CONTROLLER_URL = "$ROOT_URL/Controller";
-    $CSS_URL = "$ROOT_URL/css";
-?>
